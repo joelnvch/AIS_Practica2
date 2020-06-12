e713ce353d029a68d9f68afc689b719f68519ee9
AlejandroURJC <61578803+AlejandroURJC@users.noreply.github.com>
2020-06-12 19:42:15 +0200

    Add files via upload

--- tic-tac-toe-enunciado/src/test/java/es/codeurjc/ais/tictactoe/TicTacToeGameTest.java
+++ tic-tac-toe-enunciado/src/test/java/es/codeurjc/ais/tictactoe/TicTacToeGameTest.java
@@ -1,9 +1,17 @@
 package es.codeurjc.ais.tictactoe;
 
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+
 import es.codeurjc.ais.tictactoe.TicTacToeGame.EventType;
+import es.codeurjc.ais.tictactoe.TicTacToeGame.WinnerValue;
+
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 import static org.mockito.Mockito.*;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
@@ -11,31 +19,39 @@ import java.util.concurrent.CopyOnWriteArrayList;
 
 class TicTacToeGameTest {
 	
-	@DisplayName("El primer jugador que pone ficha gana")
-	@Test
-	void test() {
-		TicTacToeGame g = new TicTacToeGame();
+	private TicTacToeGame g;
+	private Connection c1,c2;
+	private Player p1,p2;
+	private List<Player> players;
+	
+	@BeforeEach
+	public void setupTest() {
+		g = new TicTacToeGame();
 		
-		Connection c1 = mock(Connection.class);
-		Connection c2 = mock(Connection.class);
+		c1 = mock(Connection.class);
+		c2 = mock(Connection.class);
 		
 		g.addConnection(c1);
 		g.addConnection(c2);
 		
-		Player p1 = new Player(0, "P1", "Jugador 1");
-		Player p2 = new Player(1, "P2", "Jugador 2");
+		p1 = new Player(0, "P1", "Jugador 1");
+		p2 = new Player(1, "P2", "Jugador 2");
 		
 		g.addPlayer(p1);
 		g.addPlayer(p2);
-		
-		
-		List<Player> players = new CopyOnWriteArrayList<>();
+				
+		players = new CopyOnWriteArrayList<>();
 		players.add(p1);
 		players.add(p2);
+	}
+	
+	
+	@DisplayName("El primer jugador que pone ficha gana")
+	@Test
+	void test() {		
 		verify(c1, times(2)).sendEvent(EventType.JOIN_GAME, players);
 		verify(c2, times(2)).sendEvent(EventType.JOIN_GAME, players);
-		
-		
+				
 		g.mark(0);
 		g.mark(1);
 		g.mark(3);
@@ -46,35 +62,23 @@ class TicTacToeGameTest {
 		
 		g.mark(6);
 		
-		verify(c1).sendEvent(eq(EventType.GAME_OVER), notNull());
-		verify(c2).sendEvent(eq(EventType.GAME_OVER), notNull());
+		ArgumentCaptor<WinnerValue> argumentc1 = ArgumentCaptor.forClass(WinnerValue.class);
+		ArgumentCaptor<WinnerValue> argumentc2 = ArgumentCaptor.forClass(WinnerValue.class);
+		
+		verify(c1).sendEvent(eq(EventType.GAME_OVER), argumentc1.capture());
+		verify(c2).sendEvent(eq(EventType.GAME_OVER), argumentc2.capture());
+		Object eventc1 = argumentc1.getValue();
+		Object eventc2 = argumentc2.getValue();
+		assertNotNull(eventc1);
+		assertNotNull(eventc2);
 	}
 	
 	@DisplayName("Empate")
 	@Test
-	void test1() {
-		TicTacToeGame g = new TicTacToeGame();
-		
-		Connection c1 = mock(Connection.class);
-		Connection c2 = mock(Connection.class);
-		
-		g.addConnection(c1);
-		g.addConnection(c2);
-		
-		Player p1 = new Player(0, "P1", "Jugador 1");
-		Player p2 = new Player(1, "P2", "Jugador 2");
-		
-		g.addPlayer(p1);
-		g.addPlayer(p2);
-		
-		
-		List<Player> players = new CopyOnWriteArrayList<>();
-		players.add(p1);
-		players.add(p2);
+	void test1() {		
 		verify(c1, times(2)).sendEvent(EventType.JOIN_GAME, players);
 		verify(c2, times(2)).sendEvent(EventType.JOIN_GAME, players);
-		
-		
+				
 		g.mark(0);
 		g.mark(6);
 		g.mark(4);
@@ -89,36 +93,25 @@ class TicTacToeGameTest {
 		
 		g.mark(7);
 		
-		verify(c1).sendEvent(eq(EventType.GAME_OVER), isNull());
-		verify(c2).sendEvent(eq(EventType.GAME_OVER), isNull());	
+		ArgumentCaptor<WinnerValue> argumentc1 = ArgumentCaptor.forClass(WinnerValue.class);
+		ArgumentCaptor<WinnerValue> argumentc2 = ArgumentCaptor.forClass(WinnerValue.class);
+		
+		verify(c1).sendEvent(eq(EventType.GAME_OVER), argumentc1.capture());
+		verify(c2).sendEvent(eq(EventType.GAME_OVER), argumentc2.capture());
+		
+		Object eventc1 = argumentc1.getValue();
+		Object eventc2 = argumentc2.getValue();
+		assertNull(eventc1);
+		assertNull(eventc2);	
 	}
 	
 	
 	@DisplayName("El primer jugador que pone ficha pierde")
 	@Test
-	void test2() {
-		TicTacToeGame g = new TicTacToeGame();
-		
-		Connection c1 = mock(Connection.class);
-		Connection c2 = mock(Connection.class);
-		
-		g.addConnection(c1);
-		g.addConnection(c2);
-		
-		Player p1 = new Player(0, "P1", "Jugador 1");
-		Player p2 = new Player(1, "P2", "Jugador 2");
-		
-		g.addPlayer(p1);
-		g.addPlayer(p2);
-		
-		
-		List<Player> players = new CopyOnWriteArrayList<>();
-		players.add(p1);
-		players.add(p2);
+	void test2() {		
 		verify(c1, times(2)).sendEvent(EventType.JOIN_GAME, players);
 		verify(c2, times(2)).sendEvent(EventType.JOIN_GAME, players);
-		
-		
+				
 		g.mark(0);
 		g.mark(6);
 		g.mark(4);
@@ -132,10 +125,14 @@ class TicTacToeGameTest {
 		
 		g.mark(7);
 	
-		verify(c1).sendEvent(eq(EventType.GAME_OVER), notNull());
-		verify(c2).sendEvent(eq(EventType.GAME_OVER), notNull());
-	
+		ArgumentCaptor<WinnerValue> argumentc1 = ArgumentCaptor.forClass(WinnerValue.class);
+		ArgumentCaptor<WinnerValue> argumentc2 = ArgumentCaptor.forClass(WinnerValue.class);
 		
-		
+		verify(c1).sendEvent(eq(EventType.GAME_OVER), argumentc1.capture());
+		verify(c2).sendEvent(eq(EventType.GAME_OVER), argumentc2.capture());
+		Object eventc1 = argumentc1.getValue();
+		Object eventc2 = argumentc2.getValue();
+		assertNotNull(eventc1);
+		assertNotNull(eventc2);					
 	}
 }
