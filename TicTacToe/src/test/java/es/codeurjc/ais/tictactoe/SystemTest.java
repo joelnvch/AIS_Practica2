03e5dc05564c0a9c45bf2d587d3a033205baada8
AlejandroURJC <61578803+AlejandroURJC@users.noreply.github.com>
2020-06-13 20:36:38 +0200

    Add files via upload
    SystemTest simplificado para reutilizar código

--- TicTacToe/src/test/java/es/codeurjc/ais/tictactoe/SystemTest.java
+++ TicTacToe/src/test/java/es/codeurjc/ais/tictactoe/SystemTest.java
@@ -21,18 +21,17 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 
 class SystemTest {
 
-	private WebDriver browser1;
-	private WebDriver browser2;
-	private String nombreJ1;
-	private String nombreJ2;
+	private WebDriver browser1,browser2;
+	private String nombreJ1,nombreJ2;
+	private WebDriverWait waitBoard1,waitBoard2,waitAlert1,waitAlert2;
+	private String alert1,alert2;
 	
 	
 	@BeforeAll
 	public static void setupClass() {
 		WebDriverManager.chromedriver().setup();
 		WebDriverManager.firefoxdriver().setup();
-		WebApp.start();
-		
+		WebApp.start();		
 	}
 	
 	@AfterAll
@@ -56,7 +55,8 @@ class SystemTest {
 		nickJ2.sendKeys(nombreJ2);
 		playJ1.click();
 		playJ2.click();
-		
+		waitBoard1 = new WebDriverWait(browser1, 20);
+		waitBoard2 = new WebDriverWait(browser2, 20);		
 	}
 	
 	@AfterEach
@@ -69,113 +69,72 @@ class SystemTest {
 		}
 	}
 	
-	
-	
-	
+		
 	@DisplayName("Primer jugador en poner ficha gana")
 	@Test
-	void primeroEnJugarGanaTest() {	
-		WebDriverWait waitBoard1 = new WebDriverWait(browser1, 20);
-		waitBoard1.until(ExpectedConditions.presenceOfElementLocated(By.id("cell-1")));
-		WebDriverWait waitBoard2 = new WebDriverWait(browser2, 20);
-		waitBoard2.until(ExpectedConditions.presenceOfElementLocated(By.id("cell-0")));
+	void primeroEnJugarGanaTest() {
+		int[] movimientos= {1, 0, 4, 3, 7};
+		jugar(movimientos);		
+		esperarCuadrosDeDialogo();		    		  		
+		guardarCuadrosDeDialogo();
 		
-		WebElement cell1=browser1.findElement(By.id("cell-1"));
-		cell1.click();
-		WebElement cell0=browser2.findElement(By.id("cell-0"));
-		cell0.click();
-		WebElement cell4=browser1.findElement(By.id("cell-4"));
-		cell4.click();
-		WebElement cell3=browser2.findElement(By.id("cell-3"));
-		cell3.click();
-		WebElement cell7=browser1.findElement(By.id("cell-7"));		
-		cell7.click();
+		String alertContent=nombreJ1 + " wins! " + nombreJ2 + " looses.";			
+		assertThat(alert1,equalTo(alertContent));
+		assertThat(alert2,equalTo(alertContent));												
+	}
+
 		
-		WebDriverWait waitAlert1 = new WebDriverWait(browser1, 60);
-		waitAlert1.until(ExpectedConditions.alertIsPresent());
-		WebDriverWait waitAlert2 = new WebDriverWait(browser2, 60);
-		waitAlert2.until(ExpectedConditions.alertIsPresent());
-		    		  		
-		String alert1=browser1.switchTo().alert().getText();
-		String alert2=browser2.switchTo().alert().getText();
-		String alertContent=nombreJ1 + " wins! " + nombreJ2 + " looses.";
-			
+	@DisplayName("Primer jugador en poner ficha pierde")
+	@Test
+	void primeroEnJugarPierdeTest() {	
+		int[] movimientos = {2, 0, 5, 4, 3, 8};
+		jugar(movimientos);				
+		esperarCuadrosDeDialogo();		    		  		
+		guardarCuadrosDeDialogo();
+		
+		String alertContent=nombreJ2 + " wins! " + nombreJ1 + " looses.";			
 		assertThat(alert1,equalTo(alertContent));
 		assertThat(alert2,equalTo(alertContent));												
 	}
 	
-	@DisplayName("Primer jugador en poner ficha pierde")
-	@Test
-	void primeroEnJugarPierdeTest() {
-		WebDriverWait waitBoard1 = new WebDriverWait(browser1, 20);
-		waitBoard1.until(ExpectedConditions.presenceOfElementLocated(By.id("cell-2")));
-		WebDriverWait waitBoard2 = new WebDriverWait(browser2, 20);
-		waitBoard2.until(ExpectedConditions.presenceOfElementLocated(By.id("cell-0")));
-		
-		WebElement cell2=browser1.findElement(By.id("cell-2"));
-		cell2.click();
-		WebElement cell0=browser2.findElement(By.id("cell-0"));
-		cell0.click();
-		WebElement cell5=browser1.findElement(By.id("cell-5"));
-		cell5.click();
-		WebElement cell4=browser2.findElement(By.id("cell-4"));
-		cell4.click();
-		WebElement cell3=browser1.findElement(By.id("cell-3"));		
-		cell3.click();
-		WebElement cell8=browser2.findElement(By.id("cell-8"));
-		cell8.click();
-		
-		WebDriverWait waitAlert1 = new WebDriverWait(browser1, 60);
-		waitAlert1.until(ExpectedConditions.alertIsPresent());
-		WebDriverWait waitAlert2 = new WebDriverWait(browser2, 60);
-		waitAlert2.until(ExpectedConditions.alertIsPresent());
-		    		  		
-		String alert1=browser1.switchTo().alert().getText();
-		String alert2=browser2.switchTo().alert().getText();
-		String alertContent=nombreJ2 + " wins! " + nombreJ1 + " looses.";
-			
-		assertThat(alert1,equalTo(alertContent));
-		assertThat(alert2,equalTo(alertContent));												
-	}
 	
 	@DisplayName("Los jugadores empatan")
 	@Test
 	void empateTest() {
-		WebDriverWait waitBoard1 = new WebDriverWait(browser1, 20);
-		waitBoard1.until(ExpectedConditions.presenceOfElementLocated(By.id("cell-0")));
-		WebDriverWait waitBoard2 = new WebDriverWait(browser2, 20);
-		waitBoard2.until(ExpectedConditions.presenceOfElementLocated(By.id("cell-1")));
+		int[] movimientos = {0, 1, 4, 8, 5, 6, 7, 3, 2};
+		jugar(movimientos);
+		esperarCuadrosDeDialogo();		    		  		
+		guardarCuadrosDeDialogo();
 		
-		WebElement cell0=browser1.findElement(By.id("cell-0"));
-		cell0.click();
-		WebElement cell1=browser2.findElement(By.id("cell-1"));
-		cell1.click();
-		WebElement cell4=browser1.findElement(By.id("cell-4"));
-		cell4.click();
-		WebElement cell8=browser2.findElement(By.id("cell-8"));
-		cell8.click();
-		WebElement cell5=browser1.findElement(By.id("cell-5"));		
-		cell5.click();
-		WebElement cell6=browser2.findElement(By.id("cell-6"));
-		cell6.click();
-		WebElement cell7=browser1.findElement(By.id("cell-7"));		
-		cell7.click();
-		WebElement cell3=browser2.findElement(By.id("cell-3"));
-		cell3.click();
-		WebElement cell2=browser1.findElement(By.id("cell-2"));		
-		cell2.click();
-		
-		WebDriverWait waitAlert1 = new WebDriverWait(browser1, 60);
-		waitAlert1.until(ExpectedConditions.alertIsPresent());
-		WebDriverWait waitAlert2 = new WebDriverWait(browser2, 60);
-		waitAlert2.until(ExpectedConditions.alertIsPresent());
-		    		  		
-		String alert1=browser1.switchTo().alert().getText();
-		String alert2=browser2.switchTo().alert().getText();
-		String alertContent="Draw!";
-			
+		String alertContent="Draw!";			
 		assertThat(alert1,equalTo(alertContent));
 		assertThat(alert2,equalTo(alertContent));												
+	}
+	
+	
+	private void jugar(int [] mov) {
+		waitBoard1.until(ExpectedConditions.presenceOfElementLocated(By.id("cell-" + mov[0])));		
+		waitBoard2.until(ExpectedConditions.presenceOfElementLocated(By.id("cell-" + mov[1])));
+		int cont=0;
+		for(int i:mov) {
+			if (cont%2==0) 
+				browser1.findElement(By.id("cell-" + i)).click();
+			else
+				browser2.findElement(By.id("cell-" + i)).click();
+			cont++;
+		}
+	}
+	
+	private void esperarCuadrosDeDialogo() {
+		waitAlert1 = new WebDriverWait(browser1, 60);
+		waitAlert1.until(ExpectedConditions.alertIsPresent());
+		waitAlert2 = new WebDriverWait(browser2, 60);
+		waitAlert2.until(ExpectedConditions.alertIsPresent());
+	}
+	
+	private void guardarCuadrosDeDialogo() {
+		alert1=browser1.switchTo().alert().getText();
+		alert2=browser2.switchTo().alert().getText();
 	}
 
 }
